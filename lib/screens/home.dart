dd26248e059e45aceaa47a72b5803463bf8bf31e
Ujjwal-here <ujjwalsbx@gmail.com>
2021-05-15 14:42:42 +0530

    image added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(26, 50, 43, 1),
+      ),
+      backgroundColor: Colors.white,
+    );
   }
 }
